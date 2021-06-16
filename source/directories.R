457095cf5a9cfcffa499ea13cdf7f9b0476e60eb
cottrellr <rscottrell22@gmail.com>
2021-06-16 19:11:59 +1000

    First commit

---
+++ source/directories.R
@@ -0,0 +1,8 @@
+#references location of raw data on my dropbox. Substitute these file paths for where you place the data
+
+dir_my_data <- file.path("", "Users", "rich_cottrell", "Dropbox", "github")
+dir_fao <- file.path("", "Users", "rich_cottrell", "Dropbox", "Data", "FAO")
+dir_faostat_2019 <- file.path("", "Users", "rich_cottrell", "Dropbox", "Data", "FAO", "FAOSTAT_2019")
+dir_faostat_2021 <- file.path("", "Users", "rich_cottrell", "Dropbox", "Data", "FAO", "FAOSTAT_2021")
+
+dir_production_tidy <- file.path(here("scripts", "1_tidy_production_data.Rmd"))
