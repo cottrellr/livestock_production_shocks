a630c6f83a371c6bd7f360030cf272834dd3932e
cottrellr <rscottrell22@gmail.com>
2021-06-28 12:59:46 +1000

    Fixed pig meat labelling in production data. Output first plot of time series into explore

--- scripts/2_shock_detection_function.R
+++ scripts/2_shock_detection_function.R
@@ -34,8 +34,25 @@ shock_detector_loess <- function(target_time_series, target_cooks_distance, targ
   return(shock_presence)
 }
 
-
-
+#Jessica's function
+shockid <- function(dat, thresh){
+  # dat is your time series data and threshold is the threshold you want for Cook's D (defaulted to 0.35)
+  outt <- array(dim=c(length(dat), 3))
+  x <- 1:length(dat)
+  ll <- lowess(x, dat) # Fits lowess curve (can specify other options for how the curve is estimated and can change the span)
+  rr <- as.numeric(dat[order(x)]-ll$y) #residuals off lowess
+  rrp1 <- rr[2:length(rr)] # Residuals at time t
+  rrm1 <- rr[1:(length(rr)-1)] # Residuals at time t-1
+  
+  ll2 <- lm(rrp1~rrm1) # Linear fit of the residuals
+  cd <- cooks.distance(ll2) # Calculate the Cook's D
+  
+  outt[2:length(rr),1] <- as.numeric(cd) # Output the Cook's D
+  outt[,2] <- rr # Output the residuals
+  outt[2:length(rr),3] <- ifelse(as.numeric(cd) >= thresh,1,0) # Logical of whether point is a shock
+  
+  return(outt)
+}
 
 
 
