14093431d8a5674c514d208f8b4ffc506d4ac95a
Cottrell Family <rscottrell22@gmail.com>
2021-10-01 14:00:11 +1000

    First commit after SSH key generation

--- source/directories.R
+++ source/directories.R
@@ -1,6 +1,6 @@
 #references location of raw data on my dropbox. Substitute these file paths for where you place the data
 
-dir_my_data <- file.path("", "Users", "rich_cottrell", "Dropbox", "github")
+dir_my_data <- file.path("", "Users", "cottrell", "Dropbox", "Data")
 dir_fao <- file.path("", "Users", "rich_cottrell", "Dropbox", "Data", "FAO")
 dir_faostat_2019 <- file.path("", "Users", "rich_cottrell", "Dropbox", "Data", "FAO", "FAOSTAT_2019")
 dir_faostat_2021 <- file.path("", "Users", "rich_cottrell", "Dropbox", "Data", "FAO", "FAOSTAT_2021")
