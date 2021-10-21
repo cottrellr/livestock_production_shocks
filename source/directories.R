c3cb60daf918a1b62959753f77fc18562472d1d2
Cottrell Family <rscottrell22@gmail.com>
2021-10-21 11:31:54 +1000

    changed pathways in directories following computer change and corrected maps to include only shocks(was summarising by nrows with non-shock events included before)

--- source/directories.R
+++ source/directories.R
@@ -1,8 +1,8 @@
 #references location of raw data on my dropbox. Substitute these file paths for where you place the data
 
 dir_my_data <- file.path("", "Users", "cottrell", "Dropbox", "Data")
-dir_fao <- file.path("", "Users", "rich_cottrell", "Dropbox", "Data", "FAO")
-dir_faostat_2019 <- file.path("", "Users", "rich_cottrell", "Dropbox", "Data", "FAO", "FAOSTAT_2019")
-dir_faostat_2021 <- file.path("", "Users", "rich_cottrell", "Dropbox", "Data", "FAO", "FAOSTAT_2021")
+dir_fao <- file.path("", "Users", "cottrell", "Dropbox", "Data", "FAO")
+dir_faostat_2019 <- file.path("", "Users", "cottrell", "Dropbox", "Data", "FAO", "FAOSTAT_2019")
+dir_faostat_2021 <- file.path("", "Users", "cottrell", "Dropbox", "Data", "FAO", "FAOSTAT_2021")
 
 dir_production_tidy <- file.path(here("scripts", "1_tidy_production_data.Rmd"))
